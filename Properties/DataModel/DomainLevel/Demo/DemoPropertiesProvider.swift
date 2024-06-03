4f34569fa50f7423072f406a63aec2cfa411d1b7
Vitalii Kuznetsov <v-blacksmith@hotmail.com>
2024-06-03 20:53:14 +0200

    Refactor demo properties provider

--- Properties/DataModel/DomainLevel/Demo/DemoPropertiesProvider.swift
+++ Properties/DataModel/DomainLevel/Demo/DemoPropertiesProvider.swift
@@ -13,6 +13,47 @@ final class DemoPropertiesProvider {
     var mode: DemoPropertiesProvider.Mode = .success
     
     var fetchTimeoutInSec: Double = 2
+    
+    func getLocalList() -> Result<[Property], PropertiesProviderError> {
+        if case let .failure(error) = self.mode { return .failure(error) }
+        
+        guard let url = Bundle.main.url(forResource: "demoItemsList", withExtension: "json") else {
+            return .failure(.faultyURL)
+        }
+        
+        let data: Data
+        do {
+            data = try Data(contentsOf: url)
+        } catch {
+            
+            return .failure(.resourceUnavailable)
+        }
+        do {
+            let decoder = JSONDecoder()
+            let list = try decoder.decode([Property].self, from: data)
+            
+            return .success(list)
+        } catch {
+            
+            return .failure(.decodingFailure)
+        }
+    }
+    
+    func getLocalPropertyWithID(_ propertyID: String) -> Result<Property, PropertiesProviderError> {
+        if case let .failure(error) = self.mode { return .failure(error) }
+        guard
+            case let .success(list) = self.getLocalList(),
+            let property = (list.first { $0.id == propertyID }) else { return .failure(.resourceUnavailable) }
+        
+        return .success(property)
+    }
+    
+    func getLocalPropertyWithType(_ type: PropertyType) -> Property {
+        guard case let .success(list) = self.getLocalList() else { fatalError("Local data corrupted") }
+        guard let property = (list.first { $0.type == type }) else { fatalError("Local data not found") }
+        
+        return property
+    }
 }
 
 extension DemoPropertiesProvider {
@@ -24,7 +65,7 @@ extension DemoPropertiesProvider {
         case success
         
         /// Case of failure result.
-        case fail(PropertiesProviderError)
+        case failure(PropertiesProviderError)
     }
 }
 
@@ -34,54 +75,12 @@ extension DemoPropertiesProvider: PropertiesProvider {
     func getList() async -> Result<[Property], PropertiesProviderError> {
         try? await Task.sleep(for: .seconds(self.fetchTimeoutInSec))
         
-        if case let .fail(error) = self.mode { return .failure(error) }
-        
-        guard let url = Bundle.main.url(forResource: "demoItemsList", withExtension: "json") else {
-            return .failure(.faultyURL)
-        }
-        
-        let data: Data
-        do {
-            data = try Data(contentsOf: url)
-        } catch {
-            return .failure(.resourceUnavailable)
-        }
-        
-        do {
-            let decoder = JSONDecoder()
-            let list = try decoder.decode([Property].self, from: data)
-            
-            return .success(list)
-        } catch {
-            return .failure(.decodingFailure)
-        }
+        return self.getLocalList()
     }
     
     func getPropertyWithID(_ propertyID: String) async -> Result<Property, PropertiesProviderError> {
         try? await Task.sleep(for: .seconds(self.fetchTimeoutInSec))
         
-        if case let .fail(error) = self.mode { return .failure(error) }
-        
-        guard let url = Bundle.main.url(forResource: "demoItemDetails", withExtension: "json") else {
-            return .failure(.faultyURL)
-        }
-        
-        let data: Data
-        do {
-            data = try Data(contentsOf: url)
-        } catch {
-            
-            return .failure(.resourceUnavailable)
-        }
-        
-        do {
-            let decoder = JSONDecoder()
-            let item = try decoder.decode(Property.self, from: data)
-            
-            return .success(item)
-        } catch {
-            
-            return .failure(.decodingFailure)
-        }
+        return self.getLocalPropertyWithID(propertyID)
     }
 }
